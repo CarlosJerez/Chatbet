2c13ce2b07cc86bdb603604fdf5f5bec8158dfb6
Carlos Jerez <carlosjerezquintero@gmail.com>
2024-02-03 11:35:43 -0400

    email.js implementacion

--- script.js
+++ script.js
@@ -1,25 +1,105 @@
-const form = document.getElementById("formulario")
-const nombre = document.getElementById("nombre")
-const correo = document.getElementById("correo")
-const telefono = document.getElementById("telefono")
-const pais = document.getElementById("pais")
-const comentario = document.getElementById("comentario")
+emailjs.init("85KeyZMYwckDrKqMc");
+const serviceID = "default_service";
+const templateID = "template_g3xdcte";
 
+const form = document.getElementById("formulario");
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const telefono = document.getElementById("telefono");
+const pais = document.getElementById("pais");
+const comentario = document.getElementById("comentario");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log('nombre: ',nombre.value)
-    console.log('correo: ',correo.value)
-    console.log('telefono: ',telefono.value)
-    console.log('pais: ',pais.value)
-    console.log('comentario: ',comentario.value)
+form.addEventListener("submit", enviarCorreo);
 
-    Toastify({
-        text: "This is a toast",
-        duration: 3000,
-        className: "info",
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        }
-      }).showToast();
-} )
+function enviarCorreo(e) {
+  e.preventDefault();
+  const valido = validarFormulario();
+  if (valido) {
+    enviar();
+  }
+}
+
+function validarFormulario() {
+  let validar = true;
+
+  if (!nombre.value) {
+    errorToast("Error parametro 'Nombre'");
+    nombre.classList.add("contactar-error");
+    validar = false;
+  } else {
+    nombre.classList.remove("contactar-error");
+  }
+  if (!correo.value) {
+    errorToast("Error parametro 'Correo'");
+    correo.classList.add("contactar-error");
+    validar = false;
+  } else {
+    correo.classList.remove("contactar-error");
+  }
+  if (!telefono.value) {
+    errorToast("Error parametro 'Numero de contacto'");
+    telefono.classList.add("contactar-error");
+    validar = false;
+  } else {
+    telefono.classList.remove("contactar-error");
+  }
+  if (!pais.value) {
+    errorToast("Error parametro 'Pais'");
+    pais.classList.add("contactar-error");
+    validar = false;
+  } else {
+    pais.classList.remove("contactar-error");
+  }
+  return validar;
+}
+
+function errorToast(text) {
+  Toastify({
+    text,
+    duration: 3000,
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, #b00000, #703dc9)",
+    },
+  }).showToast();
+}
+
+function succesToast(text) {
+  Toastify({
+    text,
+    duration: 3000,
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+}
+
+function resetForm() {
+  nombre.value = "";
+  correo.value = "";
+  telefono.value = "";
+  pais.value = "";
+  comentario.value = "";
+}
+
+function enviar() {
+  const templateParams = {
+    Nombre: nombre.value,
+    Correo: correo.value,
+    contacto: telefono.value,
+    pais: pais.value,
+    comentario: comentario.value,
+  };
+
+  emailjs.send(serviceID, templateID, templateParams)
+    .then(
+    () => {
+      succesToast("Formulario Enviado");
+      resetForm();
+    },
+    (err) => {
+      alert(JSON.stringify(err));
+    }
+  );
+}
